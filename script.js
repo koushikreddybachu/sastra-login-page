c95e1ea1ad1d66f2bd1f16236a329cc3fc7d84f5
koushikreddybachu <koushi.bachu@gmail.com>
2025-01-05 14:36:51 +0530

    added error aleart

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const registerNo=document.getElementById('regNo');
 const passsword=document.getElementById('pas');
 const submitButton=document.getElementById('submitDetails');
 submitButton.onclick=function (){
+    if(registerNo.value==="")
+    {
+        alert("please enter register Number");
+        return;
+    }
     let userDetails="Register No"+" : "+registerNo.value+"password"+" : "+"\n"+passsword.value;
     console.log(userDetails);
     window.location.href="https://webstream.sastra.edu/sastrapwi/";
